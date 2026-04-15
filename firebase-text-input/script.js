d89dd5994c48dfebd166983df9e0fd163907906b
yiyunh-dev <yiyunh@usc.edu>
2026-04-14 19:04:17 -0700

    n

---
+++ firebase-text-input/script.js
@@ -0,0 +1,66 @@
+// Your web app's Firebase configuration
+//const firebaseConfig = {
+//  apiKey: "AIzaSyAFajdu_Y13KgwcZA3LPJmj2j_nw7SoB0s",
+//  authDomain: "collective-input.firebaseapp.com",
+//  projectId: "collective-input",
+//  storageBucket: "collective-input.appspot.com",
+//  messagingSenderId: "338519851864",
+//  appId: "1:338519851864:web:5fb3b64d1cad63b20b1b2d",
+//  measurementId: "G-G0J7EQCZPC"
+//};
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFZvcDv7a6P07IVblTYURNPWazuwUdwDk",
+  authDomain: "iml-demo.firebaseapp.com",
+  projectId: "iml-demo",
+  storageBucket: "iml-demo.firebasestorage.app",
+  messagingSenderId: "218648189128",
+  appId: "1:218648189128:web:730c2bb54753b9a343edfd"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+let dbRef = db.ref("text");
+
+//var data ={
+//name: "q",
+//word: "hello"
+//}
+
+//dbRef.push(data);
+
+let chatContainer = document.getElementById("chat-container");
+let entry = document.getElementById("text-input-entry");
+let share = document.getElementById("text-input-submit");
+
+dbRef.on("child_added", gotText);
+
+function gotText(data) {
+  let id = data.key;
+  let value = data.val();
+  console.log(value);
+  chatContainer.innerHTML =
+    "<div class='response'>" + value + "</div>" + chatContainer.innerHTML;
+}
+
+//click button will run this function
+const textInputSubmit = document.getElementById("text-input-submit");
+textInputSubmit.addEventListener("click", submitText);
+
+let textContainerElement = document.getElementById("text-input-entry");
+
+function submitText() {
+  let textToSubmit = textContainerElement.value; //gets text value from textbox
+  let newKey = dbRef.push().key; //ask firebase to give you a new key / 'name'
+  let updates = {}; //send firebase list of values
+  updates[newKey] = textToSubmit;
+  dbRef.update(updates);
+}
+
+function submitlock() {
+  entry.remove();
+  share.value = "Thanks for telling me.";
+  share.disabled = true;
+  share.style.width = "70%";
+}
